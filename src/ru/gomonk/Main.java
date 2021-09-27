8348bbc0dc197017aa43214990435509d969f0a0
validName <validName>
2021-09-27 14:35:59 +0300

    new imit

---
+++ src/ru/gomonk/Main.java
@@ -0,0 +1,10 @@
+package ru.gomonk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float a=5,b=4;
+        float c= a*b*b/10;
+        System.out.println(c);
+    }
+}
